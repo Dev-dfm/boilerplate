aeb8d02c91cd7ca5fa9e27f20b1021e82c4ef692
Dev-dfm <d.floresmedina@gmail.com>
2021-06-07 10:32:32 +0200

    👷 Serve storybook

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+// Serve storybook production bundle
+app.use('/storybook', express.static('dist/storybook'));
+
 // Serve app production bundle
 app.use(express.static('dist/app'));
 
